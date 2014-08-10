182ec74e6f7aef044e9aaa0a575ad2558f6c8fa1
Jan Hancic <jan.hancic@gmail.com>
2014-08-10 16:11:34 +0100

    add basic components

--- client/src/components/UntitledMd.jsx
+++ client/src/components/UntitledMd.jsx
@@ -3,11 +3,18 @@
 'use strict';
 
 var React = require('react');
+var Header = require('./Header.jsx');
+var Tabs = require('./Tabs.jsx');
+var Editor = require('./Editor.jsx');
 
 module.exports = React.createClass({
 	render: function() {
 		return (
-			<div>Hello from React</div>
+			<div>
+				<Header/>
+				<Tabs/>
+				<Editor/>
+			</div>
 		);
 	}
 });
