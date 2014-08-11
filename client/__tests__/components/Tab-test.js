001b029c2980cd2fc063b9045f279d2856c4d49b
Jan Hancic <jan.hancic@gmail.com>
2014-08-11 23:29:17 +0100

    add some more tests for Tab

--- client/__tests__/components/Tab-test.js
+++ client/__tests__/components/Tab-test.js
@@ -19,4 +19,31 @@ describe('Tab component', function() {
 		expect(div.textContent).toEqual('my tab');
 		expect(div.className).toEqual('tab');
 	});
+
+	it('renders correctly if selected', function() {
+		var tab = TestUtils.renderIntoDocument(
+			<Tab name='my tab' selected={true} onClick={function() {}} />
+		);
+
+		var div = TestUtils.findRenderedDOMComponentWithTag(tab, 'div').getDOMNode();
+
+		expect(div.className).toEqual('tab selected');
+	});
+
+	it('invokes the callback when clicked', function() {
+		var didInvoke = false;
+		var cb = function() {
+			didInvoke = true;
+		};
+
+		var tab = TestUtils.renderIntoDocument(
+			<Tab name='my tab' selected={true} onClick={cb} />
+		);
+
+		var div = TestUtils.findRenderedDOMComponentWithTag(tab, 'div');
+
+		TestUtils.Simulate.click(div);
+
+		expect(didInvoke).toBe(true);
+	});
 });
