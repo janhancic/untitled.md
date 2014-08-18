5129cf09eb489107c7e5c1b8a64c67721e9770eb
Jan Hancic <jan.hancic@gmail.com>
2014-08-18 21:00:00 +0100

    md preview! note that this is using a modiffied version of showdown (I removed some node specific code that was failing in webpack)

--- client/src/components/MarkdownViewer.js
+++ client/src/components/MarkdownViewer.js
@@ -3,13 +3,20 @@
 'use strict';
 
 var React = require('react');
+var Showdown = require('showdown');
+var converter = new Showdown.converter();
 
 module.exports = React.createClass({
 	displayName: 'MarkdownViewer',
 
 	render: function() {
+		var mdHtml = converter.makeHtml(this.props.markdown);
+
 		return (
-			<div className='markdown-viewer'>{this.props.markdown}</div>
+			<div
+				className='markdown-viewer'
+				dangerouslySetInnerHTML={{__html: mdHtml}}
+			></div>
 		);
 	}
 });
